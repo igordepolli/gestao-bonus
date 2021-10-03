54bd3e869b80ce19f3e94442711e5172dbced92d
vladimirchrist <vladimir.c.pereira@gmail.com>
2021-10-03 17:23:31 -0300

    ajuste pom

--- src/test/java/br/ufes/model/EmployeeBonusTest.java
+++ src/test/java/br/ufes/model/EmployeeBonusTest.java
@@ -1,13 +1,8 @@
 package br.ufes.model;
 
 import br.ufes.calculodebonus.BonusProcessor;
-import br.ufes.model.Employee;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,26 +15,6 @@ import org.junit.jupiter.params.provider.ValueSource;
  * @author clayton
  */
 public class EmployeeBonusTest {
-
-  public EmployeeBonusTest() {
-  }
-
-  @BeforeAll
-  public static void setUpClass() {
-  }
-
-  @AfterAll
-  public static void tearDownClass() {
-  }
-
-  @BeforeEach
-  public void setUp() {
-
-  }
-
-  @AfterEach
-  public void tearDown() {
-  }
 
   @Test
   public void CT001() throws Exception {
